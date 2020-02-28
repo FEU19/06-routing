07ddd634c803bc2fc37ecac5f3c2a402ad33a744
David Andersson <david.andersson@zocom.se>
2020-02-28 14:28:28 +0100

    Finished routing example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutComponent } from './about/about.component';
 
+const routes: Routes = [
+	// exakt "/blog"
+	{ path: 'blog', component: BlogComponent, pathMatch: 'full' },
 
-const routes: Routes = [];
+	// vanlig felstavning
+	{ path: 'blogg', redirectTo: '/blog' },
+	{ path: 'blogga', redirectTo: '/blog' },
+
+	{ path: 'about', component: AboutComponent },
+
+	// { path: 'blog', component: RemovedArticleComponent },
+
+	{ path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    PageNotFoundComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
